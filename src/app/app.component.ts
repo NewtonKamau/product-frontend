7fd61d4c6ce7f3fb9264881cf0e9a18b37d40e92
Newton <kamaunewton78@gmail.com>
2018-10-27 18:50:47 +0300

    agular file structure with a few product UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgSwitch } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'catalogue';
+  title = 'Hey wassup';
+  products = [];
+
+   getProducts() {
+  fetch("http://localhost:8080/api/getProducts")
+    .then(response => {
+      // console.log(response.json());
+     return response.json()
+    }).then(data => {
+      this.products = [...data]
+      console.log(this.products);
+      
+      console.log(data);
+
+    }).catch(error => {
+      console.log(error);
+      
+    })
+
 }
+
+ngOnInit() {
+
+  this.getProducts();
+}
+  
+};
+
+
+
+
+
